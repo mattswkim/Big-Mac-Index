7469bb4498d204c265f1e95c9242f973cf1c09bd
Matt Kim <mattsunwoongkim@gmail.com>
2020-10-02 12:00:51 +0900

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
-
 const app = express();
+const bigMacIndexJson = require('./src/bigMacIndex.json')
+const fetch = require('node-fetch');
 
-app.get('/', (req, res) => {
-  const customers = [
-    {id:1, firstName: 'John', lastName: 'Doe'}
-  ];
 
-  res.json(customers);
+
+let url = "https://ipvigilante.com/json/";
+let settings = { method: "Get" };
+
+app.get('/', (req, res) => { res.send("It's working!") })
+
+app.get('/api/data', (req, res) => {  
+  let ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log("IP:", ip)
+  // using google's IP address for testing
+  fetch((url + '8.8.8.8/country_name'), settings)
+    .then(res => res.json())
+    .then((json) => {
+        // get the name of the country from the data
+        const country = json.data.country_name
+        // search for the big mac data
+        const bigMacData = bigMacIndexJson.find(({Country}) => Country === country)
+        res.json(bigMacData)
+    })
+    .catch(err => res.status(400).json('Unable to get Big Mac index data. Err: '+err));;
 })
+
+
 
 const port = 5000;
 
