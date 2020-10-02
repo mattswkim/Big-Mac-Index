9e59d556af64315bdb068139865cd450a68a483e
Matt Kim <mattsunwoongkim@gmail.com>
2020-10-02 16:58:44 +0900

    randomcoutry function added

--- server.js
+++ server.js
@@ -1,39 +1,56 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const bigMacIndexJson = require('./src/bigMacIndex.json')
-
+const cors = require('cors')
 const app = express();
-
+// filtered and cleaned Big Mac Index data from csv file
+const bigMacIndexJson = require('./src/bigMacIndex.json')
 
 let url = "https://ipvigilante.com/json/";
 let settings = { method: "Get" };
+let pos = 0;
+
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => { res.send("It's working!") })
 
 app.get('/api/data', (req, res) => {  
-  let ip =  
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  console.log("IP:", ip)
-  // using google's IP address for testing
-  fetch((url + ip + '/country_name'), settings)
+  // get client IP
+  fetch('https://api.ipify.org?format=json')
+  .then(results => results.json())
+  .then(json => {
+    console.log("IP: " + json.ip)
+    // get Country info based on cleint IP
+    fetch((url + json.ip + '/country_name'), settings)
     .then(res => res.json())
-    .then((json) => {
+    .then(json => {
         // get the name of the country from the data
         const country = json.data.country_name
-        console.log(country)
         // search for the big mac data
         const bigMacData = bigMacIndexJson.find(({Country}) => Country === country)
-        console.log("bigmac data " + bigMacData)
         res.json(bigMacData)
+        // keep the index value for the country (to avoid it to appear from random country)
+        pos = bigMacIndexJson.map((e) => { 
+          return e.Country; 
+        }).indexOf(country); 
     })
-    .catch(err => res.status(400).json('Unable to get Big Mac index data. Err: '+err));;
+  })
+    .catch(err => res.status(400).json('Unable to get Big Mac index data. Err: ' + err));;
 })
 
+app.get('/random', (req, res) => {
+  // get random nuber (there are 58 countries in the database)
+  let rand = Math.floor(Math.random() * Math.floor(58));
+  while (rand === pos) {
+    rand = Math.floor(Math.random() * Math.floor(58));
+    console.log("rand = " + rand + " pos= " + pos)
+  }
+  const data = bigMacIndexJson[rand]
+  res.json(data)
+})
 
-
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`)
